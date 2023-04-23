e59376812a20f24e31fd1297c3e4a9d9eeab4e43
Murphy <chengzhiyi_2@163.com>
2023-04-23 12:55:53 -0400

    fix

--- dataset/ppgadgets/doT/app.js
+++ dataset/ppgadgets/doT/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 var assert = require('assert');
-var doT = require('..');
-
-var logged;
+var doT = require('dot');
 
 Object.prototype.templateSettings = {varname: 'it=(console.log("executed"),{})'};
 
-const templates = doT.process({path: './examples'});
-templates.test();
+const templates = doT.process({path: './'});
+templates.test(); // Will not print "executed"
