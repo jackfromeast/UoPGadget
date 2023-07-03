2f5a1c935c131c07e2102b6e059058134119fad1
kccarlos <110118511+kccarlos@users.noreply.github.com>
2023-07-03 14:44:36 -0400

    Update 3 template engine test code

---
+++ dataset/pptestunits/vash/app-0.js
@@ -0,0 +1,24 @@
+// app.js
+const path = require('path');
+const fs = require('fs');
+const vash = require('vash');
+
+try{
+    Object._expose.setupSymbols()
+}
+catch(e){
+    console.log("[!] symbolic execution not enabled")
+}
+
+
+// Load the template
+let templatePath = path.resolve(__dirname, 'views/template.vash');
+let templateContent = fs.readFileSync(templatePath, 'utf8');
+
+// Compile the template
+let template = vash.compile(templateContent);
+
+// Render the template with some model data
+let rendered = template();
+
+console.log(rendered);
